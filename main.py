d5aed6a18931f94679739aa11e505dd29d1fc8f0
dan <dan@pop-os.localdomain>
2020-12-29 23:43:06 -0500

    added ppo (have to tweak)

--- main.py
+++ main.py
@@ -1,91 +1,57 @@
 import numpy as np
 from ppo import Agent
+import gym
+import os
 
 def main():
 
     #init the env
-        ENVS = ('Pendulum-v0', 'MountainCarContinuous-v0', 'BipedalWalker-v3', 'LunarLanderContinuous-v2',
-            'BipedalWalkerHardcore-v3')
+    ENVS = ('Pendulum-v0', 'MountainCarContinuous-v0', 'BipedalWalker-v3', 'LunarLanderContinuous-v2',
+        'BipedalWalkerHardcore-v3')
 
-    ENV = ENVS[0]
+    ENV = 'CartPole-v0'
 
-    model_dir = os.path.join(os.getcwd(), 'models')
-    os.makedirs(os.path.join(model_dir, str(datetime.date.today()) + '-' + ENV), exist_ok=True)
-    save_dir = os.path.join(model_dir, str(datetime.date.today()) + '-' + ENV)
+    # os.makedirs(os.path.join(os.getcwd(), 'models'), exist_ok=True)
+    # model_dir = os.path.join(os.getcwd(), 'models')
+    # os.makedirs(os.path.join(model_dir, str(datetime.date.today()) + '-' + ENV), exist_ok=True)
+    # save_dir = os.path.join(model_dir, str(datetime.date.today()) + '-' + ENV)
 
     env = gym.make(ENV)
     iter_per_episode = 200
-    n_state = env.observation_space.shape[0]
-    n_action = env.action_space.shape[0]
-    action_bound = env.action_space.high
+    n_state = env.observation_space.shape
+    n_action = env.action_space.n
 
     env.seed(1234)
     np.random.seed(1234)
 
     num_episodes = 1001
-    PER = False
 
-    batch_size = 64
+    batch_size = 5
     #Pendulum
-    layer_1_nodes, layer_2_nodes = 256, 128
+    layer_1_nodes, layer_2_nodes = 256, 200
 
-    tau = 0.01
     actor_lr, critic_lr = 0.0001, 0.001
     GAMMA = 0.99
-    ep = 0.001
 
-    actor_noise = OrnsteinUhlenbeck(np.zeros(n_action))
-
-    agent = DDPG(n_action, action_bound, layer_1_nodes, layer_2_nodes, actor_lr, critic_lr, PER, GAMMA,
-                 tau, batch_size, save_dir)
-
-    agent.update_target_network(agent.actor, agent.actor_target, agent.tau)
-    agent.update_target_network(agent.critic, agent.critic_target, agent.tau)
-
-    load_models = False
-    save = False
-    # If loading model, a gradient update must be called once before loading weights
-    if load_models:
-        load_model(PER, agent, batch_size, env, ep, n_action, n_state)
-
+    ppo = Agent(n_state, n_action,layer_1_nodes=layer_1_nodes,layer_2_nodes=layer_2_nodes, batch_size=64)
+    n_steps = 0
+    M = 10
     for i in range(num_episodes):
         s = env.reset()
-        sum_reward = 0
-        agent.sum_q = 0
-        agent.actor_loss = 0
-        agent.critic_loss = 0
-        j = 0
+        r = 0
+        done = False
 
-        while True:
-            env.render()
-
-            a = np.clip(agent.actor(tf.convert_to_tensor([s], dtype=tf.float32))[0] + actor_noise(), a_max=action_bound,
-                        a_min=-action_bound)
-            s1, r, done, _ = env.step(a)
-            # Store in replay memory
-            if PER:
-                error = 1 # D_i = max D
-                agent.memory.add(error, (
-                np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-            else:
-                agent.memory.add(
-                    (np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-            agent.train()
-
-            sum_reward += r
-            s = s1
-            j += 1
-            if done:
-                print(f'Episode: {i}, reward: {int(sum_reward)}, q_max: {agent.sum_q / float(j)},\nactor loss:{agent.actor_loss / float(j)}, critic loss:{agent.critic_loss/ float(j)}')
-                # rewards.append(sum_reward)
-                print('===========')
-                if save:
-                    agent.save_model()
-                break
-
-
-
-
+        while not done:            
+            prob, action, value = ppo.take_action(s)
+            s_1, reward, done, _ = env.step(action)
+            n_steps += 1
+            r += reward
+            ppo.store_memory(s, action, prob, value, reward, done)
+            if n_steps % M == 0:
+                ppo.train()
+            s = s_1
+        
+        print(f'episode: {i}, reward: {r}, steps: {n_steps}')
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,47 +1,60 @@
 import torch
 import torch.nn as nn
+from torch.distributions.categorical import Categorical
+from torch.optim import Adam
+
 
 class Actor(torch.nn.Module):
-    def __init__(self, num_state, num_actions, layer_1, layer_2, lr, checkpt):
+    def __init__(self, num_state, num_actions, layer_1, layer_2, lr=0.0001, checkpt='ppo'):
         super(Actor, self).__init__()
 
-        self.nam_state = num_state
-        self.num_actions = num_actions
-        self.layer_1 = layer_1
-        self.layer_2 = layer_2
         self.chkpt = checkpt + '_actor.ckpt'
 
         self.model = nn.Sequential(
-            nn.Linear(num_state,layer_1),
-            nn.ReLU()
-            nn.Linear(num_state,layer_1),
-            nn.ReLU()
-            nn.Linear(layer_2, num_actions)
-            nn.Softmax(ndim=-1)
+            nn.Linear(*num_state,layer_1),
+            nn.ReLU(),
+            nn.Linear(layer_1,layer_2),
+            nn.ReLU(),
+            nn.Linear(layer_2, num_actions),
+            nn.Softmax(dim=-1)
         )
+
+        self.optim = Adam(self.parameters(), lr=lr)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
 
     def forward(self,state):
         pol = self.model(state)
+        return Categorical(pol)
+
+    def save_model(self):
+        torch.save(self.state_dict(), self.chkpt)
 
 
 class Critic(torch.nn.Module):
-    def __init__(self, num_actions, layer_1, layer_2, lr, checkpt):
-        super(Actor, self).__init__()
+    def __init__(self, num_state, layer_1, layer_2, lr=0.0001, checkpt='ppo'):
+        super(Critic, self).__init__()
 
         self.nam_state = num_state
-        self.num_actions = num_actions
         self.layer_1 = layer_1
         self.layer_2 = layer_2
         self.chkpt = checkpt + '_actor.ckpt'
 
         self.model = nn.Sequential(
-            nn.Linear(num_state,layer_1),
-            nn.ReLU()
-            nn.Linear(num_state,layer_1),
-            nn.ReLU()
+            nn.Linear(*num_state,layer_1),
+            nn.ReLU(),
+            nn.Linear(layer_1,layer_2),
+            nn.ReLU(),
             nn.Linear(layer_2, 1)
         )
 
+        self.optim = Adam(self.parameters(), lr=lr)
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.to(self.device)
 
     def forward(self,state):
         Val = self.model(state)
+        return Val
+
+    def save_model(self):
+        torch.save(self.state_dict(), self.chkpt)

Fixed model saving, action std_dev calculation

torch/model.py
@@ -110,22 +110,13 @@ class ActorCNN(torch.nn.Module):
                 self.std = nn.Linear(layer_2, num_actions)
 
         else:
-            self.model = nn.Sequential(
-                nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4),
-                nn.ReLU(),
-                nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=1),
-                nn.ReLU(),
-                nn.Conv2d(in_channels=64,out_channels=64,kernel_size=3, stride=1),
-                nn.ReLU(),
-                Flatten(),
-                # nn.Linear(12 * 12 * 64,layer_1),
-                nn.Linear(7*7*64,256),
-                nn.ReLU(),
-                nn.Linear(256,448),
-                nn.ReLU(),
-                nn.Linear(layer_2, num_actions),
-                nn.Softmax(dim=-1)
-            )
+                self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=8, stride=4)
+                self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+                self.conv3 = nn.Conv2d(in_channels=64,out_channels=64,kernel_size=3, stride=1)
+                self.flat = Flatten()
+                self.fc1 = nn.Linear(4096,layer_1)
+                self.fc2 = nn.Linear(layer_1,layer_2)
+                self.mean = nn.Softmax(dim=-1)
 
         def calc_cnnweights():
             input = torch.zeros((1, 4, 96, 96))
@@ -140,15 +131,9 @@ class ActorCNN(torch.nn.Module):
             x = model(input)
             return x.shape[1]
 
-        print(calc_cnnweights())
-
         self.optim = Adam(self.parameters(), lr=lr)
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         self.to(self.device)
-
-        if self.contineous:
-            self.action_var = torch.full((num_actions,), .6 * .6).to(self.device)
-
 
 
     def forward(self,state):
@@ -157,20 +142,18 @@ class ActorCNN(torch.nn.Module):
             x = F.relu(self.conv2(x))
             x = F.relu(self.conv3(x))
             x = self.flat(x)
-            x = torch.tanh(self.fc1(x))
-            x = torch.tanh(self.fc2(x))
+            x = torch.relu(self.fc1(x))
+            x = torch.relu(self.fc2(x))
 
-            mean = self.mean(x) + 1.0
-            std_dev = self.std(x) + 1.0
-            dist = Normal(mean, std_dev)
+            mean = self.mean(x)
+            std_dev = self.std(x)
+            dist = Normal(mean, std_dev.exp())
             return dist
             
         else:
             pol = self.model(state)
             return Categorical(pol)
 
-    def save_model(self, save_dir):
-        torch.save(self.state_dict(), save_dir+'/'+self.chkpt)
 
 
 class CriticCNN(torch.nn.Module):
@@ -207,6 +190,3 @@ class CriticCNN(torch.nn.Module):
     def forward(self,state):
         Val = self.model(state)
         return Val
-
-    def save_model(self, save_dir):
-        torch.save(self.state_dict(),save_dir+'/'+self.chkpt)

torch/ppo.py
@@ -56,6 +56,7 @@ class Agent():
         self.c1 = c1
         self.gamma = .99
         self.g_lambda = 0.95
+        self.contineous = True
 
         # self.actor = Actor(num_state, num_action, layer_1_nodes, layer_2_nodes, contineous=True)
         # self.critic = Critic(num_state, layer_1_nodes, layer_2_nodes, contineous=True)
@@ -70,11 +71,14 @@ class Agent():
 
     def take_action(self,state):
         with torch.no_grad():
-            state = torch.tensor([state], dtype=torch.float).to(self.actor.device)
+            state = torch.tensor(np.array([state]), dtype=torch.float).to(self.actor.device)
             prob_dist = self.actor(state)
             value = self.critic(state)
-            action = prob_dist.sample()
-            # breakpoint()
+            if self.contineous:
+                action = prob_dist.sample()
+            else:
+                action = prob_dist.mean
+
             prob = torch.squeeze(prob_dist.log_prob(action)).cpu().detach().numpy()
             action = torch.squeeze(action).cpu().detach().numpy()
             value = torch.squeeze(value).cpu().detach().numpy()
@@ -89,6 +93,10 @@ class Agent():
 
     def store_memory(self, state,action, prob, val, reward, done):
         self.memory.store_memory(state, action, reward, val, prob, done)
+
+    def save_model(self):
+        torch.save(self.actor.state_dict(), 'models/actor.ckpt')
+        torch.save(self.critic.state_dict(), 'models/critic.ckpt')
 
     def train(self):
         epochs = 5
@@ -140,8 +148,8 @@ class Agent():
                 self.critic.optim.step()
 
         self.memory.clear_memory()
-        self.actor.save_model(self.save_dir)
-        self.critic.save_model(self.save_dir)
+        # self.actor.save_model(self.save_dir)
+        # self.critic.save_model(self.save_dir)
 
         
 